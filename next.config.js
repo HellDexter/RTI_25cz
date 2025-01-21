85627a850ee3a6590d862ec4e8fd417ea94266fd
RTI Robotech <info@rti-robotech.com>
2025-01-21 18:21:25 +0100

    Update next.config.js for image optimization on Vercel

--- next.config.js
+++ next.config.js
@@ -5,7 +5,13 @@ const nextConfig = {
   reactStrictMode: true,
   i18n,
   images: {
-    domains: ['localhost'],
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
